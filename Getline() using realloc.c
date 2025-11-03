e33fa3ff6f864ef90c9307549214942aeb9b299b
rakshi162 <rakshithahs48@gmail.com>
2025-11-03 22:06:52 +0530

    Create Getline() using realloc.c

---
+++ Getline() using realloc.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+char* getLine() {
+    char *str = NULL;     
+    int capacity = 10;   
+    int length = 0;      
+    int ch;
+    str = (char *)malloc(capacity * sizeof(char));
+    if (str == NULL) {
+        printf("Memory allocation failed!\n");
+        return NULL;
+    }
+    while ((ch = getchar()) != '\n' && ch != EOF) {
+        str[length++] = (char)ch;
+        if (length == capacity) {
+            capacity *= 2;
+            char *temp = (char *)realloc(str, capacity * sizeof(r));
+            if (temp == NULL) {
+                free(str);
+                return NULL;
+            }
+            str = temp;
+        }
+    }
+    str[length] = '\0';
+    return str; 
+}
+int main() {
+    printf("Enter a line of text: ");
+    char *line = getLine();
+    if (line == NULL) {
+        printf("Error reading input.\n");
+        return 1;
+    }
+    printf("You entered: %s\n", line);
+    free(line);
+    return 0;
+}
