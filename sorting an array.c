509d116a7cfb309fac0b04d3b44a37f45eb8918c
rakshi162 <rakshithahs48@gmail.com>
2025-11-04 21:59:24 +0530

    Create sorting an array.c

---
+++ sorting an array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int a[10]={10,50,30,20,40}, n=5, i, j, t;
+    for(i=0; i<n-1; i++)
+        for(j=i+1; j<n; j++)
+            if(a[i]>a[j]) 
+            { 
+            t=a[i];
+            a[i]=a[j]; 
+            a[j]=t; 
+            }
+    for(i=0; i<n; i++)
+    {
+    printf("%d ", a[i]);
+    }
+    return 0;
+}
