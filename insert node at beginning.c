7a7597d09c2f3358325a4917b1175525684d1dc5
rakshi162 <rakshithahs48@gmail.com>
2025-11-05 13:48:41 +0530

    Create insert node at beginning.c

---
+++ insert node at beginning.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+int data;
+struct node* next;
+};
+struct node* head=NULL;
+void insertbegin(int value){
+  struct node* newnode=malloc(sizeof(struct node));
+newnode->data=value;
+newnode->next=head;
+head=newnode;
+}
+void display(){
+  struct node* temp=head;
+while(temp!=NULL){
+printf("%d ->",temp->data);
+temp=temp->next;
+}
+}
+int main(){
+  insertbegin(10);
+insertbegin(20);
+insertbegin(30);
+display();
+return 0;
+}
