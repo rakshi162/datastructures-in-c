28231cfcab7ddd7b3eca062daac7d2577d8690d7
rakshi162 <rakshithahs48@gmail.com>
2025-11-08 14:26:29 +0530

    Create hashing operations.c

--- hashing operations.c
+++ hashing operations.c
@@ -1,64 +1,62 @@
-#include <stdio.h>
-#define SIZE 10
-int hashTable[SIZE];
-int hashFunction(int key, int size) { 
-    return key % size;
-    }
-void initHashTable() {
-    for(int i=0;i<SIZE;i++)
-    hashTable[i]=-1;
-    }
-void insert(int key) {
-    int index = hashFunction(key, SIZE);
+#include<stdio.h>
+#define size 10
+
+int hashfunc(int key){
+    return key%size;
+}
+int table[size];
+void insert(int key){
+    int index=hashfunc(key);
     int i=0;
-    while(hashTable[(index+i)%SIZE]!=-1) 
-    i++;
-    hashTable[(index+i)%SIZE]=key;
-    printf("%d inserted at index %d\n", key, (index+i)%SIZE);
-}
-void search(int key) {
-    int index=hashFunction(key,SIZE), i=0;
-    while(hashTable[(index+i)%SIZE]!=-1) {
-        if(hashTable[(index+i)%SIZE]==key) {
-            printf("%d found at index %d\n",key,(index+i)%SIZE);
-            return;
-            
-        }
+    while(table[(index+i)%size]!=-1){
         i++;
-        if(i==SIZE) 
-        break;
-    }
-    printf("%d not found\n",key);
-}
-void deleteKey(int key){
-    int index=hashFunction(key,SIZE),i=0;
-    while(hashTable[(index+i)%SIZE]!=-1){
-        if(hashTable[(index+i)%SIZE]==key){
-            hashTable[(index+i)%SIZE]=-1;
-            printf("%d deleted\n",key);
+        if(i==size){
+            printf("Table full\n");
             return;
         }
-        i++;
-        if(i==SIZE)
-        break;
     }
-    printf("%d not found to delete\n",key);
+    table[(index+i)%size]=key;
+    printf("%d inserted at index %d\n",key,index);
+}
+int search(int key){
+    int index=hashfunc(key);
+    int i=0;
+    if(table[(index+i)%size]==key){
+        printf("%d in index %d\n",key,(index+i)%key);
+        return (index+i)%key;
+    }
+    for(int i=0;i<size;i++){
+        if(table[i]==key){
+            printf("%d in index %d\n",key,i);
+            return (index+i)%key;
+        }
+        if(i==size){
+        printf("Key not found\n");
+        return 0;
+        }
+    }
+}
+void del(int key){
+    if(!search(key)){
+        return;
+    }
+    int index=search(key);
+    table[index]=-1;
+    printf("%d deleted\n",key);
 }
 void display(){
-    printf("Hash Table:\n");
-    for(int i=0;i<SIZE;i++){
-        if(hashTable[i]!=-1)
-        printf("Index %d -> %d\n",i,hashTable[i]);
-        else
-        printf("Index %d -> [empty]\n",i);
-    }
+    for(int i=0;i<size;i++)
+        printf("\n%d->%d\n",i,table[i]);
 }
 int main(){
-    initHashTable();
-    insert(5); insert(15); insert(25);
+    for(int i=0;i<size;i++){
+    table[i]=-1;
+    }
+    insert(25);
+    insert(35);
+    insert(46);
+    insert(10);
+    del(25);
+    search(35);
     display();
-    search(15);
-    deleteKey(15);
-    display();
-    return 0;
 }
